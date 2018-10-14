4cbd1fe172585e12c2e0a1a6e2ae7ad5316c6835
Zulimar Lucena <zulimar.lucena@gmail.com>
2018-10-14 13:07:29 -0500

    Compelted responsiveness

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -162,15 +162,14 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.setAttribute("id", "name");
-  name.setAttribute("class", "review-name");
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
   date.setAttribute("id", "date");
-  date.setAttribute("class", "review-name");
   li.appendChild(date);
+
 
   const rating = document.createElement('p');
   rating.setAttribute("id", "rating");
